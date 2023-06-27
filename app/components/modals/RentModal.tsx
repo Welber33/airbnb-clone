5f3af44f5501b94f6212b6bb564e496e8045b7ce
Welber33 <jwwork21@gmail.com>
2023-06-27 16:15:45 -0300

    Listing Creation Step 4 (Image Upload, Cloudinary)

--- app/components/modals/RentModal.tsx
+++ app/components/modals/RentModal.tsx
@@ -10,6 +10,7 @@ import { FieldValues, useForm } from "react-hook-form";
 import CountrySelect from "../inputs/CountrySelect";
 import dynamic from "next/dynamic";
 import Counter from "../inputs/Counter";
+import ImageUpload from "../inputs/ImageUpload";
 
 enum STEPS {
   CATEGORY = 0,
@@ -25,7 +26,7 @@ export default function RentModal() {
 
   const [step, setStep] = useState(STEPS.CATEGORY);
 
-  const { 
+  const {
     register,
     handleSubmit,
     setValue,
@@ -33,7 +34,7 @@ export default function RentModal() {
     formState: {
       errors
     },
-    reset 
+    reset
   } = useForm<FieldValues>({
     defaultValues: {
       category: '',
@@ -53,6 +54,7 @@ export default function RentModal() {
   const guestCount = watch('guestCount');
   const roomCount = watch('roomCount');
   const bathroomCount = watch('bathroomCount');
+  const imageSrc = watch('imageSrc');
 
   const Map = useMemo(() => dynamic(() => import('../Map'), {
     ssr: false
@@ -103,11 +105,11 @@ export default function RentModal() {
             key={item.label}
             className="col-span-1"
           >
-            <CategoryInput 
-             onClick={(category) => setCustomValue('category', category)}
-             selected={category === item.label}
-             label={item.label}
-             icon={item.icon}
+            <CategoryInput
+              onClick={(category) => setCustomValue('category', category)}
+              selected={category === item.label}
+              label={item.label}
+              icon={item.icon}
             />
           </div>
         ))}
@@ -115,53 +117,69 @@ export default function RentModal() {
     </div>
   )
 
-  if(step === STEPS.LOCATION) {
+  if (step === STEPS.LOCATION) {
     bodyContent = (
       <div className="flex flex-col gap-8">
-        <Heading 
+        <Heading
           title="Where is your place located?"
           subtitle="Help guests find you"
         />
 
-        <CountrySelect 
+        <CountrySelect
           value={location}
           onChange={(value) => setCustomValue('location', value)}
         />
 
-        <Map 
+        <Map
           center={location?.latlng}
         />
       </div>
     )
   }
 
-  if(step === STEPS.INFO) {
+  if (step === STEPS.INFO) {
     bodyContent = (
       <div className="flex flex-col gap-8">
-        <Heading 
+        <Heading
           title="Share some basics about your place"
           subtitle="What amenities do you have?"
         />
 
-        <Counter 
+        <Counter
           title="Guests"
           subtitle="How many guests do you allow?"
           value={guestCount}
           onChange={(value) => setCustomValue('guestCount', value)}
         />
         <hr />
-        <Counter 
+        <Counter
           title="Rooms"
           subtitle="How many rooms do you have?"
           value={roomCount}
           onChange={(value) => setCustomValue('roomCount', value)}
         />
         <hr />
-        <Counter 
+        <Counter
           title="Bathrooms"
           subtitle="How many bathrooms do you allow?"
           value={bathroomCount}
           onChange={(value) => setCustomValue('bathroomCount', value)}
+        />
+      </div>
+    )
+  }
+
+  if (step === STEPS.IMAGES) {
+    bodyContent = (
+      <div className="flex flex-col gap-8">
+        <Heading
+          title="Add a photo of your place"
+          subtitle="Show guests what your place looks like!"
+        />
+
+        <ImageUpload
+          value={imageSrc}
+          onChange={(value) => setCustomValue('imageSrc', value)}
         />
       </div>
     )
