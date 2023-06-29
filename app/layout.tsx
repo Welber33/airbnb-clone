7b225a6cb56686fd041b22b53fa830e2032cccb2
Welber33 <jwwork21@gmail.com>
2023-06-29 13:01:31 -0300

    Fetch Listings w/ Server Components(Listing Card)

--- app/layout.tsx
+++ app/layout.tsx
@@ -34,10 +34,11 @@ export default async function RootLayout({
           <RentModal />
           <LoginModal />
           <RegisterModal />
-          <Navbar currentUser={currentUser}/>
+          <Navbar currentUser={currentUser} />
         </ClientOnly>
-
-        {children}
+        <div className="pb-20 pt-28">
+          {children}
+        </div>
       </body>
     </html>
   )
